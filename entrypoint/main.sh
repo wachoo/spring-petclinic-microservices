179b2efbd37ebd89e11c0f31742bd2a16c9d2402
zhongl <zhong.lunfu@gmail.com>
2017-11-01 21:14:56 +0800

    Expose jmx port.

--- entrypoint/main.sh
+++ entrypoint/main.sh
@@ -4,7 +4,6 @@ export JAVA_OPTS="${JAVA_OPTS} -javaagent:/entrypoint/easeagent.jar=/entrypoint/
 export JAVA_OPTS="${JAVA_OPTS} -Deaseagent.log.conf=/entrypoint/log4j2.xml"
 export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
 export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote"
-export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.port=17264"
 export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"
 export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
 
