efcf39536afe2714603a7ec3914b0e1f15473bf6
zhongl <zhong.lunfu@gmail.com>
2017-11-01 20:40:46 +0800

    Enable JMX for collected.

--- entrypoint/main.sh
+++ entrypoint/main.sh
@@ -3,5 +3,9 @@
 export JAVA_OPTS="${JAVA_OPTS} -javaagent:/entrypoint/easeagent.jar=/entrypoint/application.conf"
 export JAVA_OPTS="${JAVA_OPTS} -Deaseagent.log.conf=/entrypoint/log4j2.xml"
 export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
+export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote"
+export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.port=17264"
+export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"
+export JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
 
 java ${JAVA_OPTS}  -jar /app.jar
