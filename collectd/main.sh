a85cac5fb621677cc0de11794eb346962afbfa58
zhongl <zhong.lunfu@gmail.com>
2017-11-15 14:09:52 +0800

    Fix host envs.

--- collectd/main.sh
+++ collectd/main.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-export HOSTNAME=`hostname`
-export HOST_IPV4=`hostname -I | awk '{print $1}'`
-export INTERVAL=60
-
 python conf_tmpl.py > collectd.conf
 cat collectd.conf
 /usr/share/easemon/collectd/sbin/collectd -f -C collectd.conf
