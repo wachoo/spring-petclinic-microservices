b2291f7d82379805d0c0946f074f6c93b5984a39
zhongl <zhong.lunfu@gmail.com>
2017-11-15 13:31:28 +0800

    Fix missing envs.

--- collectd/main.sh
+++ collectd/main.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export HOSTNAME=`hostname`
+export HOST_IPV4=`hostname -I | awk '{print $1}'`
+
 python conf_tmpl.py > collectd.conf
 cat collectd.conf
 /usr/share/easemon/collectd/sbin/collectd -f -C collectd.conf
